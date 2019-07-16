f3dc8aa8dc95abb7f784b2ac2e2979dd8fac0600
Azx Ster <ashusalvi281996@gmail.com>
2019-07-16 18:25:10 +0530

    Add files via upload

---
+++ phpmailer/mail-config.php
@@ -0,0 +1,46 @@
+<?php
+    // Load Composer's autoloader
+    require 'vendor/autoload.php';
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    trait mail
+    {   
+        // Mail Send function
+        function mailSend($to,$subject,$txt) {
+            $developementMode = true;
+            $mail = new PHPMailer($developementMode);
+
+            try{
+                //Server settings
+                $mail->SMTPDebug = 0;                                       // Enable verbose debug output
+                $mail->isSMTP();                                            // Set mailer to use SMTP
+                $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+                $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+                $mail->Username   = 'testassuredo@gmail.com';                     // SMTP username
+                $mail->Password   = 'Seepz@1003';                               // SMTP password
+                $mail->SMTPSecure = 'ssl';                                  // Enable TLS encryption, `ssl` also accepted
+                $mail->Port       = 465;                                    // TCP port to connect to
+
+                //Recipients
+                $mail->setFrom('testassuredo@gmail.com', 'Team AssureDo');
+                $mail->addAddress($to);     // Add a recipient      
+
+                // Content
+                $mail->isHTML(true);                                  // Set email format to HTML
+                $mail->Subject = $subject;
+                $mail->Body    = $txt;
+
+                $mail->send();
+                //echo 'Message has been sent';
+            }catch(Exception $e){
+                //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
+        }
+
+    }
+
+
+    class mainClass{
+        use mail;
+    }
